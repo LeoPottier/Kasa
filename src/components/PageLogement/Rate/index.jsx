662e7d093bba8a652bf255a53e1c7675bf773c54
Leo Pottier <leo.pottier1@outlook.fr>
2024-02-21 16:51:16 +0100

    final push

--- src/components/PageLogement/Rate/index.jsx
+++ src/components/PageLogement/Rate/index.jsx
@@ -6,10 +6,14 @@ function Rate({ note }) {
     const activeStars = Math.min(note, 5); // Nombre d'étoiles actives, limité à 5
 
     // Créer un tableau d'éléments JSX pour les étoiles actives
-    const activeStarsArray = Array(activeStars).fill(<img src={active} alt="rate active" />);
+    const activeStarsArray = Array(activeStars).fill().map((_, index) => (
+        <img key={index} src={active} alt="rate active" />
+    ));
 
     // Créer un tableau d'éléments JSX pour les étoiles inactives
-    const inactiveStarsArray = Array(5 - activeStars).fill(<img src={inactive} alt="rate inactive" />);
+    const inactiveStarsArray = Array(5 - activeStars).fill().map((_, index) => (
+        <img key={activeStars + index} src={inactive} alt="rate inactive" />
+    ));
 
     // Concaténer les tableaux d'étoiles actives et inactives
     const stars = [...activeStarsArray, ...inactiveStarsArray];
